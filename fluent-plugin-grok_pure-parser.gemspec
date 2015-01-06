9675efcd4bba656c6572e7e3387658e41dbdbba6
Alex Hornung <alex@alexhornung.com>
2015-01-06 06:31:47 +0000

    initial commit

---
+++ fluent-plugin-grok_pure-parser.gemspec
@@ -0,0 +1,26 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "fluent-plugin-grok_pure-parser"
+  spec.version       = File.read("VERSION").strip
+  spec.authors       = ["Alex Hornung"]
+  spec.email         = ["alex@alexhornung.com"]
+  spec.description   = %q{fluentd parser plugin to be able to use Grok patterns}
+  spec.summary       = spec.description
+  spec.homepage      = "https://github.com/bwalex/fluent-plugin-grok_pure-parser"
+  spec.license       = "MIT"
+  spec.has_rdoc      = false
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_dependency "jls-grok", "~> 0.11.0"
+  spec.add_dependency "fluentd", "~> 0.10.17"
+
+  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "rake", "~> 10.4.2"
+end
---
+++ lib/fluent/plugin/parser_grok_pure.rb
@@ -0,0 +1,66 @@
+require 'grok-pure'
+
+module Fluent
+  class TextParser
+    class GrokPure < Parser
+      include TypeConverter
+
+      Plugin.register_parser('grok_pure', self)
+
+      config_param :time_format, :string, :default => nil
+      config_param :time_key, :string, :default => 'time'
+      config_param :grok_pattern, :string
+      config_param :grok_pattern_path, :string, :default => nil
+
+      unless method_defined?(:log)
+        define_method(:log) { $log }
+      end
+
+      class PatternError < ParserError; end
+
+      def initialize
+        super
+        @time_parser = TimeParser.new(@time_format)
+        @mutex = Mutex.new
+        @grok = Grok.new
+      end
+
+      def configure(conf)
+        super
+
+        if @grok_pattern_path
+          Dir["#{@grok_pattern_path}/*"].each do |f|
+            @grok.add_patterns_from_file(f)
+          end
+        end
+
+        begin
+          @grok.compile(@grok_pattern, true)
+        rescue Grok::PatternError => e
+          raise PatternError, e.message
+        end
+      end
+
+      def parse(text)
+        time = nil
+        record = {}
+
+        matched = @grok.match_and_capture(text) do |k,v|
+          if k == @time_key
+            time = @mutex.synchronize { @time_parser.parse(v) }
+          else
+            record[k] = @type_converters.nil? ? v : convert_type(k,v)
+          end
+        end
+
+        if matched
+          time ||= Engine.now if @estimate_current_event
+          yield time, record
+        else
+          yield nil, nil
+        end
+
+      end
+    end
+  end
+end
